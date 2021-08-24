bd4a05efbc813faa5aa461680369c251f786996c
chebon <chebon85@gmail.com>
2021-08-24 18:37:39 +0300

    restart serve

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/css/argon-design-system-react.css";
-
 import App from './App';
 
 ReactDOM.render(
