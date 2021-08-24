eb15ca8d6ab0e28ae86253b77c48e393fda8fdd2
chebon <chebon85@gmail.com>
2021-08-24 18:50:52 +0300

    production bug  resolution

--- src/portfolio.js
+++ src/portfolio.js
@@ -27,7 +27,7 @@ export const socialLinks = {
 
 export const skillsSection = {
     title: "What I do",
-    subTitle: "CURIOUS FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+    subTitle: "CURIOUS FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK.",
     skills: [
         emoji(
             "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
